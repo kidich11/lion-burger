26696fbac9e5e0cb4a30d170cad3dbaef4410587
kidich11 <p0nomarevlion@yandex.ru>
2025-10-25 09:46:27 +0300

    update

--- js/index.js
+++ js/index.js
@@ -1,7 +1,13 @@
 let mobBtn = document.querySelector('#mobile-btn');
 let closeMobNav = document.querySelector("#close_mob_nav")
 let mobNav = document.querySelector(".mobile-nav");
+let shopBtn = document.querySelector('#shopping-cart');
+let userBtn = document.querySelector('#user');
 
+
+function handleNotification(){
+    alert('На данный момент находится в разработке 😊')
+}
 
 mobBtn.onclick = () => {
     mobNav.style.display = 'flex';
@@ -10,4 +16,12 @@ mobBtn.onclick = () => {
 
 closeMobNav.onclick = () => {
     mobNav.style.display = 'none';
+}
+
+shopBtn.onclick = () => {
+    handleNotification();
+}
+
+userBtn.onclick = () => {
+    handleNotification();
 }
